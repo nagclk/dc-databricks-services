58c220a70abc885dd2b53a1cd2e7c751bfc8fb57
nagaraja konana veeranna <nveeranna@altimetrik.com>
2025-09-19 13:34:20 +0530

    initial commit

--- modules/group/main.tf
+++ modules/group/main.tf
@@ -1,12 +1,12 @@
 terraform {
-required_providers {
-azuread = {
-source = "hashicorp/azuread"
-version = "~>2"
-}
-databricks = {
-sourc = "databricks/databricks"
-version = "~>1"
-}
-}
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>2"
+    }
+    databricks = {
+      sourc   = "databricks/databricks"
+      version = "~>1"
+    }
+  }
 }
